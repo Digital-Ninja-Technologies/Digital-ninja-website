b1b19c48f2679c50e479d65dfc2ae626605c2409
Ifeoluwa Onifade <onifadeifeoluwa1@gmail.com>
2026-06-12 06:58:57 +0000

    feat: add Tawk.to live chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -332,6 +332,23 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(faqSchema) }}
           strategy="beforeInteractive"
         />
+        <Script
+          id="tawkto"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/63eb9d3c4742512879134869/1gp85sdmj';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
         <LoadingWrapper>
           <Navbar />
           <div className="mt-20">{children}</div>
